ee5433433ebb28efbb8399871e95b56927feeac1
Albine k Jibu <albinekjibu57@gmail.com>
2025-02-28 12:35:55 +0530

    check file

--- expensetrackapp/views.py
+++ expensetrackapp/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'pages-register.html', {'form': form})
+
+
+def first(request):
+    return render(request, 'first.html')
+
+def pages_login(request):
+    return render(request, 'pages-login.html')
+
+def pages_register(request):
+    return render(request, 'pages-register.html')
