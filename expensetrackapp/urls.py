b94078da8e852316d6943fa5397f9f9993dd12d2
Albine k Jibu <albinekjibu57@gmail.com>
2025-03-04 17:28:19 +0530

    check

--- expensetrackapp/urls.py
+++ expensetrackapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='loginuser'),  # Custom login URL
+    path('', views.login_user, name='loginuser'),  # Custom login URL
     path('register/', views.signup, name='register'),
     path('index/', views.index, name='index'),
     path('add-expense/', views.add_expense, name='add-expense'),
